d85601cd01389ffc03b05bebf31bb60630ec65c1
cipa-dev <risal9719@gmail.com>
2026-04-04 15:12:14 +0800

    pt4

--- post-test/post-test-apl-4/2509106004-SYIFASITIAULIAANWAR-PT-4.cpp
+++ post-test/post-test-apl-4/2509106004-SYIFASITIAULIAANWAR-PT-4.cpp
@@ -8,24 +8,21 @@ struct User{
     string username;
     string password;
 };
-
 struct Produk{
     string nama;
     string warna;
     int harga;
     int stok;
 };
-
-// fungsi
-bool inputAngka(int &var){
-    if(!(cin >> var)){
+bool inputAngka(int &x){
+    cin >> x;
+    if(cin.fail()){
         cin.clear();
         cin.ignore(1000,'\n');
         return false;
     }
     return true;
 }
-
 void tampilProduk(Produk *produk, int jumlahProduk);
 void tambahProduk(Produk *produk, int *jumlahProduk);
 void ubahProduk(Produk *produk, int jumlahProduk);
@@ -33,45 +30,51 @@ void hapusProduk(Produk *produk, int *jumlahProduk);
 bool login(User *user, int jumlahUser);
 void registrasi(User *user, int *jumlahUser);
 int totalStok(Produk *produk, int n);
-
+void garis(); 
+void garis(int n);
 int main(){
     User user[100];
     Produk produk[100];
-
     int jumlahUser = 0;
     int jumlahProduk = 0;
     int menuUtama;
-
     do{
         system("cls");
-        cout<<"MENU UTAMA\n";
-        cout<<"1. Login\n2. Registrasi\n3. Keluar\n";
-        cout<<"Pilih: ";
-
+        cout<<"-------------------------------"<<endl;
+        cout<<"         MENU UTAMA            "<<endl;
+        cout<<"-------------------------------"<<endl;
+        cout<<"1. login"<<endl;
+        cout<<"2. registrasi"<<endl;
+        cout<<"3. keluar"<<endl;
+        cout<<"pilih menu : ";
         if(!inputAngka(menuUtama)){
-            cout<<"Input harus angka!\n";
+            cout<<"input harus angka"<<endl;
             system("pause");
             continue;
         }
-
         if(menuUtama==1){
             if(login(user,jumlahUser)){
                 int menuAdmin;
                 do{
                     system("cls");
-                    cout<<"MENU ADMIN\n";
-                    cout<<"1. Tampil\n2. Tambah\n3. Ubah\n4. Hapus\n5. Keluar\n";
-                    cout<<"Pilih: ";
-
+                    cout<<"--------------------------------------"<<endl;
+                    cout<<"        MENU ADMIN SYIFA HIJAB        "<<endl;
+                    cout<<"--------------------------------------"<<endl;
+                    cout<<"1. tampilkan produk"<<endl;
+                    cout<<"2. tambah produk"<<endl;
+                    cout<<"3. ubah produk"<<endl;
+                    cout<<"4. hapus produk"<<endl;
+                    cout<<"5. keluar"<<endl;
+                    cout<<"pilih menu : ";
                     if(!inputAngka(menuAdmin)){
-                        cout<<"Input harus angka!\n";
+                        cout<<"input harus angka"<<endl;
                         system("pause");
                         continue;
                     }
-
                     if(menuAdmin==1){
                         tampilProduk(produk,jumlahProduk);
-                        cout<<"Total stok: "<<totalStok(produk,jumlahProduk)<<endl;
+                        int total = totalStok(produk,jumlahProduk);
+                        cout<<"total stok semua produk : "<<total<<endl;
                         system("pause");
                     }
                     else if(menuAdmin==2){
@@ -83,165 +86,152 @@ int main(){
                     else if(menuAdmin==4){
                         hapusProduk(produk,&jumlahProduk);
                     }
-
                 }while(menuAdmin!=5);
             }
         }
         else if(menuUtama==2){
             registrasi(user,&jumlahUser);
         }
-
     }while(menuUtama!=3);
-
-    cout<<"Terima kasih\n";
+    cout<<"terima kasih"<<endl;
 }
-
-// LOGIN
 bool login(User *user, int jumlahUser){
     string username,password;
-
-    cout<<"Username: "; cin>>username;
-    cout<<"Password: "; cin>>password;
-
-    if(username=="admincipa" && password=="004")
-        return true;
-
-    for(int i=0;i<jumlahUser;i++){
-        if(username==user[i].username && password==user[i].password)
+    int kesempatan = 3;
+    while(kesempatan>0){
+        system("cls");
+        cout<<"----------- LOGIN -----------"<<endl;
+        cout<<"username : ";
+        cin>>username;
+        cout<<"password : ";
+        cin>>password;
+        if(username=="admincipa" && password=="004"){
             return true;
+        }
+        for(int i=0;i<jumlahUser;i++){
+            if(username==user[i].username && password==user[i].password){
+                return true;
+            }
+        }
+        kesempatan--;
+        cout<<"login gagal, sisa kesempatan : "<<kesempatan<<endl;
+        system("pause");
     }
-
-    cout<<"Login gagal!\n";
-    system("pause");
     return false;
 }
-
-// REGISTRASI
 void registrasi(User *user, int *jumlahUser){
     system("cls");
-
-    cout<<"Username: ";
+    cout<<"----------- REGISTRASI -----------"<<endl;
+    cout<<"username : ";
     cin>>user[*jumlahUser].username;
-
-    cout<<"Password: ";
+    cout<<"password : ";
     cin>>user[*jumlahUser].password;
-
     (*jumlahUser)++;
-
-    cout<<"Registrasi berhasil\n";
+    cout<<"registrasi berhasil"<<endl;
     system("pause");
 }
-
-// TAMPIL
 void tampilProduk(Produk *produk, int jumlahProduk){
     system("cls");
-
+    cout<<"----------------- DAFTAR PRODUK -----------------"<<endl;
     if(jumlahProduk==0){
-        cout<<"Belum ada produk\n";
+        cout<<"belum ada produk"<<endl;
         return;
     }
-
+    garis(55);
     cout<<left<<setw(5)<<"No"
         <<setw(20)<<"Nama"
+        <<setw(15)<<"Warna"
         <<setw(10)<<"Harga"
         <<setw(5)<<"Stok"<<endl;
-
+    garis(55);
     for(int i=0;i<jumlahProduk;i++){
-        cout<<setw(5)<<i+1
+        cout<<left<<setw(5)<<i+1
             <<setw(20)<<produk[i].nama
+            <<setw(15)<<produk[i].warna
             <<setw(10)<<produk[i].harga
             <<setw(5)<<produk[i].stok<<endl;
     }
+    garis(55);
 }
-
-// TAMBAH
 void tambahProduk(Produk *produk, int *jumlahProduk){
     system("cls");
-
+    cout<<"------ tambah produk ------"<<endl;
     cin.ignore();
-    cout<<"Nama: ";
+    cout<<"nama produk : ";
     getline(cin,produk[*jumlahProduk].nama);
-
-    cout<<"Harga: ";
+    cout<<"warna : ";
+    getline(cin,produk[*jumlahProduk].warna);
+    cout<<"harga : ";
     while(!inputAngka(produk[*jumlahProduk].harga)){
-        cout<<"Harus angka! ";
+        cout<<"harus angka : ";
     }
-
-    cout<<"Stok: ";
+    cout<<"stok : ";
     while(!inputAngka(produk[*jumlahProduk].stok)){
-        cout<<"Harus angka! ";
+        cout<<"harus angka : ";
     }
-
     (*jumlahProduk)++;
-
-    cout<<"Berhasil tambah\n";
+    cout<<"produk berhasil ditambahkan"<<endl;
     system("pause");
 }
-
-// UBAH
 void ubahProduk(Produk *produk, int jumlahProduk){
     tampilProduk(produk,jumlahProduk);
-
     int pilih;
-    cout<<"Pilih: ";
-
+    cout<<"pilih nomor produk : ";
     if(!inputAngka(pilih)){
-        cout<<"Harus angka!\n";
+        cout<<"input harus angka"<<endl;
         system("pause");
         return;
     }
-
     if(pilih>=1 && pilih<=jumlahProduk){
         cin.ignore();
-
-        cout<<"Nama baru: ";
+        cout<<"nama baru : ";
         getline(cin,produk[pilih-1].nama);
-
-        cout<<"Harga: ";
+        cout<<"warna baru : ";
+        getline(cin,produk[pilih-1].warna);
+        cout<<"harga baru : ";
         inputAngka(produk[pilih-1].harga);
-
-        cout<<"Stok: ";
+        cout<<"stok baru : ";
         inputAngka(produk[pilih-1].stok);
-
-        cout<<"Berhasil ubah\n";
+        cout<<"produk berhasil diubah"<<endl;
     }
     else{
-        cout<<"Tidak valid\n";
+        cout<<"nomor tidak valid"<<endl;
     }
-
     system("pause");
 }
-
-// HAPUS
 void hapusProduk(Produk *produk, int *jumlahProduk){
     tampilProduk(produk,*jumlahProduk);
-
     int hapus;
-    cout<<"Pilih: ";
-
+    cout<<"pilih nomor produk : ";
     if(!inputAngka(hapus)){
-        cout<<"Harus angka!\n";
+        cout<<"input harus angka"<<endl;
         system("pause");
         return;
     }
-
     if(hapus>=1 && hapus<=*jumlahProduk){
         for(int i=hapus-1;i<(*jumlahProduk)-1;i++){
             produk[i]=produk[i+1];
         }
         (*jumlahProduk)--;
-
-        cout<<"Berhasil hapus\n";
+        cout<<"produk berhasil dihapus"<<endl;
     }
     else{
-        cout<<"Tidak valid\n";
+        cout<<"nomor tidak valid"<<endl;
     }
-
     system("pause");
 }
-
-// REKURSIF
 int totalStok(Produk *produk, int n){
-    if(n==0) return 0;
+    if(n==0){
+        return 0;
+    }
     return produk[n-1].stok + totalStok(produk,n-1);
+}
+void garis(){
+    cout<<"-----------------------------"<<endl;
+}
+void garis(int n){
+    for(int i=0;i<n;i++){
+        cout<<"-";
+    }
+    cout<<endl;
 }
